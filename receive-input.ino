4157e550d37ee038501993ae6f8193cbf13e2237
Arthur Geel <geelaj01@gmail.com>
2018-06-02 20:11:06 +0200

    Rewrote the code so that it facilitates the lighting behaviour for the three Arcade buttons
    Still //todo: add in ring light support

--- receive-input.ino
+++ receive-input.ino
@@ -1,27 +1,77 @@
 /* This program makes the Arduino listen to Serial Monitor data sent by the Raspberry Pi. 
-   The built-in LED on the Arduino board blinks with a certain interval. Adjust the piInput
-   variable from the Raspberry Pi to adjust the blinking speed.
-   
+   The LEDs inside three arcade buttons can be controlled through the RPI-Arduino 
+   connection. It has four states: inactive, little news, lotsa news and music playback.
+
    Original code by Fabio Nelli, https://www.meccanismocomplesso.org/en/controlling-arduino-raspberry-pi/
    Adapted by Arthur Geel, 02-06-2018
 */
 
-const int ledPin = 13; // built-in LED on Arduino board
+const int led_button_1 = 9;
+const int led_button_2 = 10;
+const int led_button_3 = 11;
+
+int brightness = 0;
+int fadeAmount = 5;
 int piInput;
 
 void setup() {
-  pinMode(ledPin, OUTPUT);
+  pinMode(led_button_1, OUTPUT);
+  pinMode(led_button_2, OUTPUT);
+  pinMode(led_button_3, OUTPUT);
   Serial.begin(9600);
-  piInput = 7;
+  piInput = 0;
 }
 
 void loop(){;
-  if(Serial.available()){
-    // listens to serial monitor for input sent by Raspberry Pi
+  if (Serial.available()){
     piInput = Serial.read() - '0';
   }
-  digitalWrite(ledPin, HIGH);
-  delay(piInput * 100);
-  digitalWrite(ledPin, LOW);
-  delay(piInput * 100);
+  
+  if (piInput == 1) {
+    analogWrite(led_button_1, 0);
+    analogWrite(led_button_2, 0);
+    analogWrite(led_button_3, 0);
+  }
+  
+  else if (piInput == 2) {
+    analogWrite(led_button_1, brightness);
+    analogWrite(led_button_2, brightness);
+    analogWrite(led_button_3, brightness);
+    
+    brightness = brightness + fadeAmount;
+    
+    if (brightness <= 0 || brightness >= 255) {
+      fadeAmount = -fadeAmount;
+    }
+    
+    delay(50);
+  }
+  
+  else if (piInput == 3) {
+    analogWrite(led_button_1, brightness);
+    analogWrite(led_button_2, brightness);
+    analogWrite(led_button_3, brightness);
+    
+    brightness = brightness + fadeAmount;
+    
+    if (brightness <= 0 || brightness >= 255) {
+      fadeAmount = -fadeAmount;
+    }
+    
+    delay(10);
+    
+  }
+  
+  else if (piInput == 4) {
+    analogWrite(led_button_1, 255);
+    analogWrite(led_button_2, 255);
+    analogWrite(led_button_3, 255);
+    
+  }
+  
+  else {
+    analogWrite(led_button_1, 0);
+    analogWrite(led_button_2, 0);
+    analogWrite(led_button_3, 0);
+  }
 }
