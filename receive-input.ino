e57d3d47d83d722591ae7256b78526c9c1af10c2
Arthur Geel <geelaj01@gmail.com>
2018-06-03 17:19:49 +0200

    Got rid of Delay( ) in favour of Millis( )
    To my dearest friend Delay( ):
    
    "May the chocolate chips in your cookies always turn out to be raisins."

--- receive-input.ino
+++ receive-input.ino
@@ -1,77 +1,163 @@
 /* This program makes the Arduino listen to Serial Monitor data sent by the Raspberry Pi. 
    The LEDs inside three arcade buttons can be controlled through the RPI-Arduino 
-   connection. It has four states: inactive, little news, lotsa news and music playback.
+   connection. It has a number of states of (in)activeness.
 
-   Original code by Fabio Nelli, https://www.meccanismocomplesso.org/en/controlling-arduino-raspberry-pi/
+   Original millis()-based code by James Lewis, as pwm-fade-with-millis.ino 
+   see baldengineer.com/fading-led-analogwrite-millis-example.html for the source code
+   
+   Original controlling Arduino with RPI code by Fabio Nelli
+   see meccanismocomplesso.org/en/controlling-arduino-raspberry-pi/ for the source code
+   
    Adapted by Arthur Geel, 02-06-2018
 */
+ 
+const byte LED_BUTTON_1 = 9;
+const byte LED_BUTTON_2 = 10;
+const byte LED_BUTTON_3 = 11;
 
-const int led_button_1 = 9;
-const int led_button_2 = 10;
-const int led_button_3 = 11;
-
-int brightness = 0;
-int fadeAmount = 5;
 int piInput;
-
+ 
+ 
+// define directions for LED fade
+#define UP 0
+#define DOWN 1
+ 
+// constants for min and max PWM
+const int minPWM = 0;
+const int maxPWM = 255;
+ 
+// State Variable for Fade Direction
+byte fadeDirection = UP;
+ 
+// Global Fade Value
+// but be bigger than byte and signed, for rollover
+int fadeValue = 0;
+ 
+// How smooth to fade?
+byte fadeIncrement = 5;
+ 
+// millis() timing Variable, just for fading
+unsigned long previousFadeMillis;
+ 
+// How fast to increment?
+int fadeInterval = 50;
+int fastFadeInterval = 20;
+ 
 void setup() {
-  pinMode(led_button_1, OUTPUT);
-  pinMode(led_button_2, OUTPUT);
-  pinMode(led_button_3, OUTPUT);
+  // put LED_BUTTONs into known state (off)
+  analogWrite(LED_BUTTON_1, 0); 
+  analogWrite(LED_BUTTON_2, 0); 
+  analogWrite(LED_BUTTON_3, 0); 
+  
   Serial.begin(9600);
   piInput = 0;
 }
+ 
+void regularFade(unsigned long thisMillis) {
+  // is it time to update yet?
+  // if not, nothing happens
+  if (thisMillis - previousFadeMillis >= fadeInterval) {
+    // yup, it's time!
+    if (fadeDirection == UP) {
+      fadeValue = fadeValue + fadeIncrement;  
+      if (fadeValue >= maxPWM) {
+        // At max, limit and change direction
+        fadeValue = maxPWM;
+        fadeDirection = DOWN;
+      }
+    } else {
+      //if we aren't going up, we're going down
+      fadeValue = fadeValue - fadeIncrement;
+      if (fadeValue <= minPWM) {
+        // At min, limit and change direction
+        fadeValue = minPWM;
+        fadeDirection = UP;
+      }
+    }
+    // Only need to update when it changes
+    analogWrite(LED_BUTTON_1, fadeValue);
+    analogWrite(LED_BUTTON_2, fadeValue); 
+    analogWrite(LED_BUTTON_3, fadeValue);   
+ 
+    // reset millis for the next iteration (fade timer only)
+    previousFadeMillis = thisMillis;
+  }
+}
 
-void loop(){;
+void fastFade(unsigned long thisMillis) {
+  // is it time to update yet?
+  // if not, nothing happens
+  if (thisMillis - previousFadeMillis >= fastFadeInterval) {
+    // yup, it's time!
+    if (fadeDirection == UP) {
+      fadeValue = fadeValue + fadeIncrement;  
+      if (fadeValue >= maxPWM) {
+        // At max, limit and change direction
+        fadeValue = maxPWM;
+        fadeDirection = DOWN;
+      }
+    } else {
+      //if we aren't going up, we're going down
+      fadeValue = fadeValue - fadeIncrement;
+      if (fadeValue <= minPWM) {
+        // At min, limit and change direction
+        fadeValue = minPWM;
+        fadeDirection = UP;
+      }
+    }
+    // Only need to update when it changes
+    analogWrite(LED_BUTTON_1, fadeValue);
+    analogWrite(LED_BUTTON_2, fadeValue); 
+    analogWrite(LED_BUTTON_3, fadeValue);   
+ 
+    // reset millis for the next iteration (fade timer only)
+    previousFadeMillis = thisMillis;
+  }
+}
+
+void buttonsOn(){
+  analogWrite(LED_BUTTON_1, 255);
+  analogWrite(LED_BUTTON_2, 255); 
+  analogWrite(LED_BUTTON_3, 255);  
+}
+
+void buttonsOff(){
+  analogWrite(LED_BUTTON_1, 0);
+  analogWrite(LED_BUTTON_2, 0); 
+  analogWrite(LED_BUTTON_3, 0);  
+}
+ 
+void loop() {
+  // get the current time, for this time around loop
+  // all millis() timer checks will use this time stamp
+  unsigned long currentMillis = millis();
+  
+  
   if (Serial.available()){
     piInput = Serial.read() - '0';
   }
-  
+
   if (piInput == 1) {
-    analogWrite(led_button_1, 0);
-    analogWrite(led_button_2, 0);
-    analogWrite(led_button_3, 0);
+    buttonsOff();
   }
   
   else if (piInput == 2) {
-    analogWrite(led_button_1, brightness);
-    analogWrite(led_button_2, brightness);
-    analogWrite(led_button_3, brightness);
-    
-    brightness = brightness + fadeAmount;
-    
-    if (brightness <= 0 || brightness >= 255) {
-      fadeAmount = -fadeAmount;
-    }
-    
-    delay(50);
+    regularFade(currentMillis);
+    // Slow pulsing buttons. Subtle ring animation.
   }
   
   else if (piInput == 3) {
-    analogWrite(led_button_1, brightness);
-    analogWrite(led_button_2, brightness);
-    analogWrite(led_button_3, brightness);
-    
-    brightness = brightness + fadeAmount;
-    
-    if (brightness <= 0 || brightness >= 255) {
-      fadeAmount = -fadeAmount;
-    }
-    
-    delay(10);
-    
+    fastFade(currentMillis); 
+    // Fast pulsing button. Prominent ring animations.
   }
   
   else if (piInput == 4) {
-    analogWrite(led_button_1, 255);
-    analogWrite(led_button_2, 255);
-    analogWrite(led_button_3, 255);
-    
+    fastFade(currentMillis);
+    // buttons gently pulse, LED ring should indicate current time and overall time.
   }
   
   else {
-    analogWrite(led_button_1, 0);
-    analogWrite(led_button_2, 0);
-    analogWrite(led_button_3, 0);
+    buttonsOff();
+    // catch incorrect data: this will turn the buttons off.
   }
 }
